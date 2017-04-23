cb685b6362dddd94b1313b3390ed9ca61786071e
hiroyuki-seki <hiroyuki-seki@users.noreply.github.com>
2017-04-23 16:12:50 +0900

    studying vim
    - ignores files generated by vim.
    - uses Gnu Grep on Windows.
    - appends configurations.

--- vim/launch-vim.bat
+++ vim/launch-vim.bat
@@ -1,5 +1,5 @@
 set DEVROOT=V:\develop
-set PATH=%PATH%;%DEVROOT%\ctags58
+set PATH=%PATH%;%DEVROOT%\ctags58;%ProgramFiles(x86)%\GnuWin32\bin
 call gvim -g ^
   -S "%USERPROFILE%\vimfiles\Session.vim" ^
   -u "%DEVROOT%\hryky-codebase\vim\vimrc"
