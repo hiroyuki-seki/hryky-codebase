35d1996fedb4e96de83bfe1f863cf60e550edb0f
hiroyuki-seki <hiroyuki-seki@users.noreply.github.com>
2017-04-25 22:03:12 +0900

    loads plugin

--- vim/launch-vim.bat
+++ vim/launch-vim.bat
@@ -2,6 +2,5 @@ set DEVROOT=V:\develop
 set PATH=%PATH%;%DEVROOT%\ctags58;%ProgramFiles(x86)%\GnuWin32\bin
 call gvim -g ^
   -S "%USERPROFILE%\vimfiles\Session.vim" ^
-  -u "%DEVROOT%\hryky-codebase\vim\vimrc"
 
 
