840091222ffde5be7bf614a290447f94bd99748f
hiroyuki-seki <hiroyuki-seki@users.noreply.github.com>
2017-05-03 16:50:43 +0900

    starts to use texlib.

--- vim/launch-vim.bat
+++ vim/launch-vim.bat
@@ -1,6 +1,6 @@
 set DEVROOT=V:\develop
-set PATH=%PATH%;%DEVROOT%\ctags58;%ProgramFiles(x86)%\GnuWin32\bin
+set PATH=%DEVROOT%\texlive\2016\bin\win32;%PATH%;%DEVROOT%\ctags58;%ProgramFiles(x86)%\GnuWin32\bin
 call gvim -g ^
-  -S "%USERPROFILE%\vimfiles\Session.vim" ^
+	-S "%USERPROFILE%\vimfiles\Session.vim" ^
 
 
